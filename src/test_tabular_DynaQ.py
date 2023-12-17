84f27eb334f53d56617b9a1e2f6d52018b1ee3e8
Nic Leblanc <106348280+nicolasleblanc-poly@users.noreply.github.com>
2023-12-16 21:11:00 -0500

    Generated graphs

--- src/test_tabular_DynaQ.py
+++ src/test_tabular_DynaQ.py
@@ -5,7 +5,8 @@ from algorithms.tabular_dynaQ import Tabular_DynaQ
 from envs.discrete_gridworld import DiscreteGridWorld
 from envs.discrete_gridworld import StochasticDiscreteGridWorld
 
-# env = DiscreteGridWorld(size=(10, 10), seed=42)
+
+# # env = StochasticDiscreteGridWorld(size=(10, 10))
 # env.reset()
 # env.render()
 
@@ -28,19 +29,24 @@ from envs.discrete_gridworld import StochasticDiscreteGridWorld
 # plt.show()
 
 
-
-env = StochasticDiscreteGridWorld(size=(10, 10))
+env = DiscreteGridWorld(size=(10, 10), seed=42)
+# env = StochasticDiscreteGridWorld(size=(10, 10))
 env.reset()
 env.render()
 
 dynaQ = Tabular_DynaQ(env)
 
-dynaQ.training(50)
-total_rewards, nb_steps_episodes = dynaQ.eval()
-
+total_rewards, nb_steps_episodes = dynaQ.training()
 print(total_rewards)
 print(np.mean(total_rewards))
 print(nb_steps_episodes)
+
+dynaQ.eval()
+# total_rewards, nb_steps_episodes = dynaQ.eval()
+# print(total_rewards)
+# print(np.mean(total_rewards))
+# print(nb_steps_episodes)
+
 
 # Final graph
 dynaQ.render_q_values()
