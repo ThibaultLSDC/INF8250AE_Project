61094c0f296ac802d6e30b753e8e604dd26eb2c2
unknown <thibault.de.chezelles@gmail.com>
2023-12-16 23:09:28 -0500

    DQN final version with trained model ckpt/metrics
    Bunch of fixes, tricks to make the learning more stable:
    - Fix on done values (= reward)
    - Smaller model (for speed mostly)
    - Last layer of q net initialized at 0
    - Larger target update delay
    - Larger batch
    - Added batchnorm
    - .train now returns a list with all the losses
    - 2000 random actions before training
    
    HP:
    env:DiscreteContinuousGridWorld(render_mode='human', size=(3, 3), wall_positions=((((1.5, 1), (1.5, 3)),)))
    model:DQN(env, epsilon=.1, buffer_capacity=100000)
    trained over 300k steps
    
    otherwise default up to this commit

--- src/algorithms/dqn.py
+++ src/algorithms/dqn.py
@@ -18,17 +18,24 @@ class DQN():
         self.buffer = deque(maxlen=buffer_capacity)
 
         self.q_network = self.build_network()
+
+        # initialize last layer to 0
+        self.q_network[-1].weight.data.zero_()
+        self.q_network[-1].bias.data.zero_()
+
         self.update_target()
         self.opt = torch.optim.Adam(self.q_network.parameters(), lr=0.001)
 
     def build_network(self):
         input_dimension = self.env.reset().shape[0]
         return nn.Sequential(
-            nn.Linear(input_dimension, 256),
+            nn.Linear(input_dimension, 128),
             nn.ReLU(),
-            nn.Linear(256, 256),
+            nn.BatchNorm1d(128),
+            nn.Linear(128, 128),
             nn.ReLU(),
-            nn.Linear(256, self.env.action_space.n)
+            nn.BatchNorm1d(128),
+            nn.Linear(128, self.env.action_space.n)
         )
     
     def update_target(self):
@@ -38,7 +45,7 @@ class DQN():
         if not hasattr(self, 'target_counter'):
             self.target_counter = 0
         self.target_counter += 1
-        if self.target_counter % 1000 == 0:
+        if self.target_counter % 500 == 0:
             self.target_network = deepcopy(self.q_network)
     
     def process_state(self, state):
@@ -47,17 +54,19 @@ class DQN():
     def update_buffer(self, state, action, reward, next_state):
         self.buffer.append((state, action, reward, next_state))
     
-    def get_batch(self, batch_size=32):
+    def get_batch(self, batch_size=256):
         batch = random.sample(self.buffer, batch_size)
         state, action, reward, next_state = zip(*batch)
         return np.array(state), np.array(action)[:, None], np.array(reward)[:, None], np.array(next_state)
 
     def act(self, state, greedy=False):
+        self.q_network.eval()
         if not greedy:
             if np.random.random() < self.epsilon:
                 return np.random.randint(0, self.env.action_space.n)
-        state = torch.tensor(state, dtype=torch.float32)
+        state = torch.tensor(state, dtype=torch.float32)[None, :]
         q_values = self.q_network(state)
+        self.q_network.train()
         return int(torch.argmax(q_values).cpu().detach().numpy())
 
     def learn_q(self, state, action, reward, next_state):
@@ -65,14 +74,14 @@ class DQN():
         action = torch.tensor(action, dtype=torch.long)
         reward = torch.tensor(reward, dtype=torch.float32)
         next_state = torch.tensor(next_state, dtype=torch.float32)
-        done = 1 - reward
+        done = reward
 
         value = self.q_network(state).gather(1, action)
         with torch.no_grad():
             next_value = self.target_network(next_state)
-            max_next_value = torch.max(next_value, dim=1)[0][:, None]
+            max_next_value = next_value.max(dim=1, keepdims=True).values
             target = reward + (1-done) * max_next_value * self.discount
-        loss = nn.MSELoss()(value, target)
+        loss = nn.MSELoss()(value, target.detach())
         self.opt.zero_grad()
         loss.backward()
         self.opt.step()
@@ -83,8 +92,9 @@ class DQN():
         state = self.process_state(state)
         done = False
         counter = tqdm(range(steps))
+        losses = []
         for step in counter:
-            if step < 10000:
+            if step < 2000:
                 action = self.env.action_space.sample()
             else:
                 action = self.act(state)
@@ -97,12 +107,14 @@ class DQN():
                 state = self.env.reset()
                 state = self.process_state(state)
 
-            if len(self.buffer) > 10000:
+            if len(self.buffer) > 2000:
                 # self.env.render()
                 batch = self.get_batch()
                 loss_q = self.learn_q(*batch)
+                losses.append(loss_q)
                 self.update_target()
-                counter.set_description(f"Q loss: {loss_q:.4f}")
+                counter.set_description(f"Q loss: {sum(losses[-100:]) / len(losses[-100:]):.5f}")
+        return losses
     
     def test(self, steps=10000):
         state = self.env.reset()
